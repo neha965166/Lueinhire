81aa6ce270035db736c02d12832b1cf23afe9b61
neha965166 <107788397+neha965166@users.noreply.github.com>
2025-01-25 17:15:23 +0530

    added to upload files

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.getElementById('start-button').addEventListener('click', () => {
     alert("Welcome to LueinHire! Let's get started on your career journey.");
-  });
+  }); 
+function scrollToSection(sectionId) {
+    document.querySelector(sectionId).scrollIntoView({ behavior: "smooth" });
+  }
